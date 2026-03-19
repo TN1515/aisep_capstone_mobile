8a05352948a272d42e2240003e1d115857ca04a3
TN1515 <tmb14112004@gmail.com>
2026-03-19 10:28:41 +0700

    first commit

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const String appTitle = 'Flutter MVVM App';
+  static const String homeTitle = 'Flutter MVVM Demo';
+  static const String pushMessage = 'You have pushed the button this many times:';
+  static const String incrementTooltip = 'Increment';
+}
---
+++ lib/features/counter/views/counter_view.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import '../view_models/counter_view_model.dart';
+import '../../../core/constants/app_strings.dart';
+
+class CounterView extends StatefulWidget {
+  const CounterView({super.key});
+
+  @override
+  State<CounterView> createState() => _CounterViewState();
+}
+
+class _CounterViewState extends State<CounterView> {
+  final CounterViewModel _viewModel = CounterViewModel();
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppStrings.homeTitle),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(AppStrings.pushMessage),
+            ListenableBuilder(
+              listenable: _viewModel,
+              builder: (BuildContext context, Widget? child) {
+                return Text(
+                  '${_viewModel.counterValue}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _viewModel.incrementCounter,
+        tooltip: AppStrings.incrementTooltip,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
